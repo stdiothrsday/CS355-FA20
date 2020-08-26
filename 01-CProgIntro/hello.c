e38152c489630c861d03340fb3036bedf29eb579
Stan Kurkovsky <stankurkovsky@gmail.com>
2020-08-26 17:17:38 +0000

    Added a "Hello, world" example

---
+++ 01-CProgIntro/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Your first ever program in C
+
+int main() {                    // serves as the entry point into any C program
+    printf("Hello, world!\n");
+    return 0;                   // indicates a successful completion of the program
+}
